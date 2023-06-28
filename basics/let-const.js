3190905fcbceaa288f015dc8bd00ec2803164688
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-28 23:50:25 +0300

    let-const

---
+++ basics/let-const.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var name = 'John Doe';
+
+const username = {
+  name: 'Andrew',
+  email: 'andrew@mail.com'
+};
+
+username.name = 'John';
+username.email = 'john@mail.com';
+
+function init() {
+  if (true) {
+   document.getElementById('output').innerHTML = `${username.name} ${username.email}`;
+  }
+
+}
+
+init();
