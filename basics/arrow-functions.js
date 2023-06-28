2edb5d37af36400746cf33e37db3a41568eeed20
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-29 00:22:23 +0300

    class-in-session

--- basics/arrow-functions.js
+++ basics/arrow-functions.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const init = () => {
-  document.getElementById('output').innerHTML = 'Hello World!';
+document.getElementById('output').innerHTML = 'Hello World!';
 };
 
 function nameString(firstName, lastName) {
-  return `${firstName} ${lastName}`;
+return `${firstName} ${lastName}`;
 }
 
 let users = [
-  {
-    name: 'John',
-    lastName: 'Doe',
-  },
-  {
-    name: 'Dennis',
-    lastName: 'Ivanov',
-  },
+{
+name: 'John',
+lastName: 'Doe',
+},
+{
+name: 'Dennis',
+lastName: 'Ivanov',
+},
 ];
 
 let names = [];
 
 // users.forEach(function(user) {
-//   console.log(nameString(user.firstName, user.lastName));
-//   names.push(user.name);
+// console.log(nameString(user.firstName, user.lastName));
+// names.push(user.name);
 // });
 
 users.forEach(user => names.push(user.name));
@@ -32,8 +32,8 @@ document.getElementById('output').innerHTML = names.join(', ');
 
 document.getElementById('btn').addEventListener('click', (e) =>
 {
-  const getDetails = () => {
-    return `The button id is ${e.currentTarget.getAttribute('id')}`;
-  }
-    document.getElementById('output').innerHTML = getDetails();
-  });
+const getDetails = () => {
+return `The button id is ${e.currentTarget.getAttribute('id')}`;
+}
+document.getElementById('output').innerHTML = getDetails();
+});
---
+++ basics/class-session.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Initialize an Item class
+
+// function Item(name, category) {
+//     this.name = name;
+//     this.category = category;
+// }
+
+// // Create a method called getDetails() for an Item
+// Item.prototype.getDetails = function () {
+//     return `${this.name} - ${this.category}`;
+// }
+
+// // Initialize a PurchasedItem class, inherits the Item class
+// function PurchasedItem(name, category, price) {
+//   Item.call(this, name, category);
+//   this.price = price;
+// }
+
+// // Sets the inherited methos and properties of the base class
+// PurchasedItem.prototype = Object.create(Item.prototype);
+// PurchasedItem.prototype.constructor = PurchasedItem;
+
+// // Create a new method just for the extended PurchasedItem class
+// PurchasedItem.prototype.getDetailsWithPrice = function () {
+//     return `${this.name} - ${this.category} - $${this.price}`;
+// }
+
+// var item = new Item('Coffe', 'Food');
+
+// item.category = 'Drinks';
+
+// var purchasedItem = new PurchasedItem('Coffe', 'Drinks', '2.49');
+
+class Item {
+    constructor(name, category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    static maxItems = 10;
+
+    static getHelperText() {
+      return 'Add some items to your grocery list';
+
+    }
+
+
+    getDetails() {
+        return `${this.name} - ${this.category}`;
+    }
+}
+
+class PurchasedItem extends Item {
+    constructor(name, category, price) {
+        super(name, category);
+        this.price = price;
+    }
+
+    getDetailsWithPrice() {
+        return `${this.name} - ${this.category} - $${this.price}`;
+    }
+
+    static getNumberOfItems() {
+      return `3 / ${super.maxItems}`;
+    }
+}
+
+// let item =  new Item('Coffe', 'Food');
+// item.category = 'Drinks';
+
+// let purchasedItem = new PurchasedItem('Coffe', 'Drinks', '2.49');
+
+// document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice();
+
+document.getElementById('output').innerHTML = PurchasedItem.getNumberOfItems();
