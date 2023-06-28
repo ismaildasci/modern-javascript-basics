3d287ce127db8c2d060151ac96367b9236f7452a
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-29 01:05:40 +0300

    maps-sets

--- basics/async-await.js
+++ basics/async-await.js
@@ -1,13 +1,13 @@
 'use strict';
 
 async function init() {
-  const start = Date.now();
-  document.getElementById('output').innerHTML = `0: init()`;
+const start = Date.now();
+document.getElementById('output').innerHTML = `0: init()`;
 
- const userPromise = getUserData();
- const welcomeStringPromise = getWelcomeString();
+const userPromise = getUserData();
+const welcomeStringPromise = getWelcomeString();
 
-const user  = await userPromise;
+const user = await userPromise;
 document.getElementById('output').innerHTML += `<br>${Date.now() - start}: ${user.name} (${user.email})`;
 
 const welcomeString = await welcomeStringPromise;
@@ -18,27 +18,27 @@ document.getElementById('output').innerHTML += `<br>${Date.now() - start}: ${wel
 
 function getUserData() {
 
-  return new Promise((resolve, reject) => {
+return new Promise((resolve, reject) => {
 
-    let user = {
-      name: 'Andre',
-      email: 'andre@mail.com'
-    };
+let user = {
+name: 'Andre',
+email: 'andre@mail.com'
+};
 
-    setTimeout( () => {
-      resolve(user);
-    }, 2000);
-  });
+setTimeout( () => {
+resolve(user);
+}, 2000);
+});
 }
 
 function getWelcomeString() {
 
-  return new Promise((resolve, reject) => {
+return new Promise((resolve, reject) => {
 
-    setTimeout( () => {
-      resolve('Welcome to my website!');
-    },2000);
-  });
+setTimeout( () => {
+resolve('Welcome to my website!');
+},2000);
+});
 }
 
 init();
---
+++ basics/maps-sets.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// const fruit = new Map();
+
+// fruit.set(1, 'Banana');
+// fruit.set(2, 'Apple');
+// fruit.set(3, 'Orange');
+
+// document.getElementById('output').innerHTML = fruit.get(1);
+
+// fruit.delete(2);
+
+// // fruit.forEach(item => {
+// document.getElementById('output').innerHTML += `<li>${fruit.get(1)}</li>`;
+// });
+
+const users = new Map();
+
+users.set({
+'name': 'Andre',
+'email': 'andre@mail.com'
+}, function(user){
+document.getElementById('output').innerHTML += `<li>${user.name} (${user.email})</li>`;
+});
+
+const userAshley = {
+'name': 'Ashley',
+'email': 'ashley@mail.com'
+};
+
+
+users.set(userAshley, function(user){
+document.getElementById('output').innerHTML += `<li>${user.name} (${user.email})</li>`;
+});
+
+
+users.forEach((value,key) => {
+value(key);
+});
+
+let userAshleyExists = users.has(userAshley);
+
+console.log('userAshleyExists', userAshleyExists);
+
+const fruit = new Set();
+
+fruit.add('Banana');
+fruit.add('Apple');
+fruit.add('Orange');
+
+fruit.forEach(value => {
+  document.getElementById('output').innerHTML += `<li>${value}</li>`;
+});
